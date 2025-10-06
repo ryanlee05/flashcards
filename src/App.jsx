92ccb2f4df0aca1a6b7ca752240f0b2d9050ca31
ryanlee05 <ryanworld2005@icloud.com>
2025-10-05 21:57:48 -0400

    flashcards 2 changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,24 +8,97 @@ function App() {
 
   const [flipped, setFlipped] = useState(false);
 
-  const showNextCard = () => {
-    setCurrentIndex(prevIndex => (prevIndex + 1) % cards.length);
-    setFlipped(false);
+  const [leftEnd, setLeftEnd] = useState(true);
+  const [rightEnd, setRightEnd] = useState(false);
+
+  const [userAnswer, setUserAnswer] = useState("");
+
+  const [background, setBackground] = useState("white");
+
+  const compareAnswers = (e) => {
+    e.preventDefault();
+    if (userAnswer.toLowerCase() === cards[currentIndex].answer.toLowerCase()) {
+      setBackground("right");
+    }
+    else {
+      setBackground("wrong");
+      setUserAnswer("");
+    }
+  }
+
+  const shuffleCards = () => {
+    const shuffled = [...cards];
+    for (let i = shuffled.length -1; i >0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const holder = shuffled[i];
+      shuffled[i] = shuffled[j];
+      shuffled[j] = holder;
+    }
+    setCards(shuffled);
+    setCurrentIndex(0);
+    setLeftEnd(true);
+    setRightEnd(false);
+    return shuffled;
+  }
+
+  const showLeftCard = () => {
+    setBackground("white");
+    setUserAnswer("");
+    //check if the next card is the beginning of the array
+    if (currentIndex -1 === 0) {
+      //if it is, set the index to the beginning and set leftend to true
+      setCurrentIndex(prevIndex => prevIndex - 1);
+      setLeftEnd(true); 
+      setFlipped(false);
+    }
+    //check if the current index is not the beginning of the array
+    else if (currentIndex -1 !== -1) {
+      //if it is not, decrement the index by 1 and set leftend to false
+      setCurrentIndex(prevIndex => prevIndex - 1);
+      setFlipped(false);
+      setLeftEnd(false);
+      setRightEnd(false);
+    }
+    //if the current index is the beginning of the array, set leftend to true
+    else {
+      setLeftEnd(true);
+    }
+  }
+
+  const showRightCard = () => {
+    setBackground("white");
+    setUserAnswer("");
+    if (currentIndex + 1 === cards.length -1) {
+      setCurrentIndex(prevIndex => prevIndex + 1);
+      setRightEnd(true);
+      setFlipped(false);
+    }
+    else if (currentIndex + 1 !== cards.length) {
+      setCurrentIndex(prevIndex => prevIndex + 1);
+      setRightEnd(false);
+      setFlipped(false);
+      setLeftEnd(false);
+    }
+    else {
+      setRightEnd(true);
+    }
   }
 
 
-  const cards = [
+  const [cards, setCards] = useState([
     { description: "What is the final score to shoot par on a course?", answer: "72"}, 
     { description: "What is the term for a score of three under par on a single hole?", answer: "Albatross" },
     { description: "What is the term for a score of two under par on a single hole?", answer: "Eagle" },
     { description: "What is the term for a score of one under par on a single hole?", answer: "Birdie" },
     { description: "What is the term for a score of one over par on a single hole?", answer: "Bogey" },
     { description: "What is the term for a score of two over par on a single hole?", answer: "Double Bogey" },
-    { description: "What is the term for hitting the ball into the hole in one stroke from the tee box?", answer: "Hole-in-One" },
+    { description: "What is the term for hitting the ball into the hole in one stroke from the tee box?", answer: "Hole In One" },
     { description: "What is the term for a shot that curves sharply to the right (for a right-handed golfer)?", answer: "Slice" },
     { description: "What is the term for a shot that curves sharply to the left (for a right-handed golfer)?", answer: "Hook" },
     { description: "What club do you use when you are closest to the green?", answer: "Putter" }
-  ]
+  ]);
+
+
 
 
   return (
@@ -37,8 +110,21 @@ function App() {
           <h3>Total flashcards: 10</h3>
           <SingleFlashcard flipped = {flipped} setFlipped = {setFlipped} description = {cards[currentIndex].description} answer = {cards[currentIndex].answer}/>
         </div>
+        <div className = "form">
+          <p>Guess the answer here: </p>
+          <form onSubmit = {compareAnswers}>
+            <input  className = {background}
+            type = "text"
+            value = {userAnswer}
+            onChange = {(e) => setUserAnswer(e.target.value)}
+            />
+          </form>
+          <button onClick = {compareAnswers}>Submit</button>
+        </div>
         <div className = "buttons">
-          <button onClick = {showNextCard}>Next</button>
+          <button className = {`button ${leftEnd ? "blur" : ""}`} onClick = {showLeftCard}>Left</button>
+          <button className = {`button ${rightEnd ? "blur" : ""}`} onClick = {showRightCard}>Right</button>
+          <button onClick = {shuffleCards} >Shuffle Cards</button>
         </div>
       </div>
     </>
